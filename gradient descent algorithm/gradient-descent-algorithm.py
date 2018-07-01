9ed92716dd615b0544352623be19501ea4e6976f
Fabiana Boldrin <bibacb.fb@gmail.com>
2018-07-01 19:47:41 -0300

    def method for calculating the error

--- gradient descent algorithm/gradient-descent-algorithm.py
+++ gradient descent algorithm/gradient-descent-algorithm.py
@@ -1,3 +1,15 @@
 # good exemple of calcutaling m and b with gradient descent
 # in this exemple, x = distance a person bikes and y = amout of calories they lost.
 # also, m and b are values of the following equation: y = m.x + b
+
+import numpy as np
+
+# m is slope, b is y-intercept
+
+def compute_error_for_line_given_points(b, m, points):
+    totalError = 0
+    for i n range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        totalError += (y - (m * x + b)) ** 2
+    return totalError / float(len(points))
