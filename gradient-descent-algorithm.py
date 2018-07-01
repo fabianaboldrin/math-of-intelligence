cc538ffad5591b74150206a0103fbadebaed614d
Fabiana Boldrin <bibacb.fb@gmail.com>
2018-07-01 19:05:51 -0300

    explanation

---
+++ gradient-descent-algorithm.py
@@ -0,0 +1,3 @@
+# good exemple of calcutaling m and b with gradient descent
+# in this exemple, x = distance a person bikes and y = amout of calories they lost.
+# also, m and b are values of the following equation: y = m.x + b
