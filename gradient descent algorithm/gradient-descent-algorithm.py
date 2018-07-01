b677e7713338b3bae087bd2f1edfc216eebfe145
Fabiana Boldrin <bibacb.fb@gmail.com>
2018-07-01 19:59:01 -0300

    add method for calculating the gradient

--- gradient descent algorithm/gradient-descent-algorithm.py
+++ gradient descent algorithm/gradient-descent-algorithm.py
@@ -13,3 +13,16 @@ def compute_error_for_line_given_points(b, m, points):
         y = points[i, 1]
         totalError += (y - (m * x + b)) ** 2
     return totalError / float(len(points))
+
+def step_gradient(b_current, m_current, points, learningRating):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2 / N) * (y - ((m_gradient * x) + m_gradient))
+    new_b = b_current - (learningRating * b_gradient)
+    new_m = m_current - (learningRating * m_gradient)
+    return [new_b, new_m]
