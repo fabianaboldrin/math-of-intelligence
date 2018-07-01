4be67ade5c82c831ef9b9823907cd51c78604bba
Fabiana Boldrin <bibacb.fb@gmail.com>
2018-07-01 20:06:30 -0300

    run() method

--- gradient descent algorithm/gradient-descent-algorithm.py
+++ gradient descent algorithm/gradient-descent-algorithm.py
@@ -26,3 +26,32 @@ def step_gradient(b_current, m_current, points, learningRating):
     new_b = b_current - (learningRating * b_gradient)
     new_m = m_current - (learningRating * m_gradient)
     return [new_b, new_m]
+
+def gradient_descent_runner(points, starting_b. starting_m, learning_rate, num_interations):
+    b = starting_b
+    m = starting_m
+    for i in range(num_interations):
+        b, m = step_gradient(b, m, array(points), learning_rate)
+    return [b, m]
+
+def run():
+    points = genfromtxt('data.csv', delimiter = ',')
+    learning_rate = 0.0001
+    initial_b = 0 # initial y-intercept guess
+    inicial_m = 0 # initial slope guess
+    num_iterations = 1000
+    print
+    "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m,
+                                                                        compute_error_for_line_given_points(initial_b,
+                                                                                                            initial_m,
+                                                                                                            points))
+    print
+    "Running..."
+    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
+    print
+    "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m,
+                                                                compute_error_for_line_given_points(b, m, points))
+
+    if __name__ == '__main__':
+        run()
+
